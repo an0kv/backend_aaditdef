6f059ed24c3d7ca42f64f9be70a4ad9dca80054f
an0kv <dcbbhhbfcbhgv@gmail.com>
2025-04-01 12:14:02 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -10,6 +10,6 @@ func LoadConfig() *Config {
 	return &Config{
 		MongoURI:  "mongodb://localhost:27017",
 		JWTSecret: "your_jwt_secret_key",
-		VTAPIKey:  "your_virustotal_api_key",
+		VTAPIKey:  "y96184c01603734518ff2ac91435bc1e6b5fa2c679be650dc49895b4425fc3c25",
 	}
 }
